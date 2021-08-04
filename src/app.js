085519b9776d2b759d70745ec16f1f6560e0ff04
Dennis lam <denwt.lam@outlook.com>
2021-08-04 14:41:31 -0400

    use partials of handlebars

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
+
 const app = express();
 
 console.log(__dirname);
@@ -7,11 +9,15 @@ console.log(path.join(__dirname, '../public/'));
 
 //defined paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public/');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 //set up handlebars 
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+//setup static directory
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
