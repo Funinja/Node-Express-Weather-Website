42729b08d26fff24db4182a98cac0f15a20e97f6
Dennis lam <denwt.lam@outlook.com>
2021-08-05 11:54:27 -0400

    added humidity setting

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -5,13 +5,13 @@ const forecast = (longitude, latitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=0a9275d4360d2bc6950b120763fbede6&query='+ longitude + ','+ latitude +'&units=m';
     
     request({url, json: true}, (error, response) =>{
-        const {weather_descriptions, temperature, feelslike} = response.body.current;
+        const {weather_descriptions, temperature, feelslike, humidity} = response.body.current;
         if (error){
             callback("Unable to connect to weather services", undefined);
         }else if(response.body.error){
             callback("Unable to find coordinates", undefined);
         }else{
-            const data = weather_descriptions[0] + ". It is currently " + temperature + " degrees out. It feels like " + feelslike + " degrees out.";  
+            const data = weather_descriptions[0] + ". It is currently " + temperature + " degrees out. It feels like " + feelslike + " degrees out. The humidity is " + humidity + "%.";  
             callback(undefined, data);
         }
 
